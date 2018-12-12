a7c3243d19539a8912aeb16e7801a08a6b583cab
Wilfried <wilfried.dias@etu.univ-amu.fr>
2018-12-12 10:27:07 +0100

    macash

--- src/JS/javascript.js
+++ src/JS/javascript.js
@@ -2,15 +2,21 @@ function resizing(){
   document.getElementById("column-hook").style.height=window.innerHeight+'px';
 }
 
-function getJSON(){
-  alert("I'm in !");
-  var requestURL = './JSON/fiche.json';
-  var request = new XMLHttpRequest();
-  request.open('GET', requestURL);
-  request.responseType = 'json';
-  request.send();
-  request.onload = function() {
-    var jsonObj = request.response;
-    alert(jsonObj);
-  }
+function getJSON(path, success, error){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success){
+                  success(JSON.parse(xhr.responseText));
+                }
+            } else {
+                if (error){
+                  error(xhr);
+                }
+            }
+        }
+    };
+    xhr.open("GET", Chrome.extension.getURL(path), true);
+    xhr.send();
 }
