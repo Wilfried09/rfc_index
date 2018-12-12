ecff9d9711b5fce19875c228d6dcfa42e0f159de
Wilfried <wilfried.dias@etu.univ-amu.fr>
2018-12-12 17:09:03 +0100

    Json working

--- src/JS/javascript.js
+++ src/JS/javascript.js
@@ -2,6 +2,18 @@ function resizing(){
   document.getElementById("column-hook").style.height=window.innerHeight+'px';
 }
 
+function errors(data){
+  console.log('Erreur')
+  console.log(data)
+
+
+}
+function success(data) {
+  console.log("Success !");
+  createNav(data);
+  createFiche(data);
+ }
+
 function getJSON(path, success, error){
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
@@ -17,6 +29,22 @@ function getJSON(path, success, error){
             }
         }
     };
-    xhr.open("GET", Chrome.extension.getURL(path), true);
+    xhr.open("GET", path, true);
     xhr.send();
+}
+
+function createNav(data){;
+  let result = "" ;
+  var nav = data.index.linkFiches;
+  console.log("nav : "+nav);
+  for(var obj in nav){
+    console.log(nav[obj]);
+    result = ''+result+"<a href='"+nav[obj].link+"'><span class='btn btn-link'>"+nav[obj].name+"</span></a>";
+  }
+  console.log(result);
+  document.getElementById("nav").innerHTML=result;
+}
+
+function createFiche(data){
+  console.log(data);
 }
